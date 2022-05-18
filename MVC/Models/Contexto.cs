94e08db91e825f2d2a3ec9de1eb1527bf39ef4bd
wandlangamer <wand_langamer@hotmail.com>
2022-05-18 14:13:33 -0300

    CRUD em arquitetura MCV no Visual Studio - C#

---
+++ MVC/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC.Models
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+    }
+}
---
+++ MVC/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { set; get; }
+        public String Nome { set; get; }
+        public String Email { set; get; }
+    }
+}
